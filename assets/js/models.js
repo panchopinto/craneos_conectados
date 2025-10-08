721624c36d8fd01a8bc1d0efc6f5ba7eb25e663e
panchopinto <69988761+panchopinto@users.noreply.github.com>
2025-10-08 08:20:00 -0300

    Add files via upload

---
+++ assets/js/models.js
@@ -0,0 +1,10 @@
+<script>
+// Rellena con tus modelos reales:
+window.MODELS = [
+  {"path":"assets/models/lontra_felina.glb","label":"🦦 lontra felina"},
+  {"path":"assets/models/lycalopex_fulvipes_1607.glb","label":"🦊 lycalopex fulvipes"},
+  {"path":"assets/models/cephalorhynchus_eutropia_delfin.glb","label":"🐬 delfín chileno"},
+  {"path":"assets/models/ctenomys_fulvus_1113.glb","label":"🐹 ctenomys fulvus"},
+  {"path":"assets/models/octodon_degus.glb","label":"🐭 octodon degus"}
+];
+</script>
